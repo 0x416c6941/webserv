1cafe141cae235add9fedec6d102b683bdf17f43
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-06-14 16:56:47 +0200

    HTTPRequest: Final class definition, implementation of process_info(), set_method()

--- include/HTTPRequest.hpp
+++ include/HTTPRequest.hpp
@@ -27,19 +27,16 @@ class HTTPRequest
 		/**
 		 * Processes line in \p info
 		 * until the field terminator ("\r\n") is met:
-		 * sets the request method (as well as `_request_target` and `_query` fields),
-		 * appends the `_header_fields` with the new field,
+		 * sets the request method (as well as `_request_target` and `_request_query` fields),
+		 * or appends the `_header_fields` with the new field,
 		 * or marks the request as complete (fully parsed).
 		 * @brief	Process info stored in \p info.
 		 * @throw	invalid_argument	\p info isn't terminated
 		 * 					or contains invalid information.
-		 * @throw	runtime_error		Header fields started
-		 * 					before method was set,
-		 * 					or request begins with "\r\n".
 		 * @throw	range_error		Request was fully parsed,
 		 * 					yet new information arrived.
 		 * @param	info	Line with information to process.
-		 * @return	Processed bytes in \p info.
+		 * @return	Processed bytes in \p info (including "\r\n").
 		 */
 		size_t process_info(const std::string &info);
 
@@ -49,6 +46,21 @@ class HTTPRequest
 		 * @return	Method of the request.
 		 */
 		enum e_method get_method() const;
+
+		/**
+		 * Get the request target.
+		 * @throw	runtime_error	Request target wasn't set yet.
+		 * @return	Request target.
+		 */
+		const std::string &get_request_target() const;
+
+		/**
+		 * Get the request query.
+		 * If request query wasn't present, empty string is returned.
+		 * @throw	runtime_error	Request query wasn't set yet.
+		 * @return	Request query (empty string if wasn't present).
+		 */
+		const std::string &get_request_query() const;
 
 		/**
 		 * Get the value of a header with the \p key.
@@ -68,17 +80,19 @@ class HTTPRequest
 
 	private:
 		enum e_method _method;
-		bool _method_set;		// To check if `_method` is initialized.
+		bool _method_is_set;		// To check if `_method` is initialized.
 		std::string _request_target;
-		std::string _query;		// Optional.
+		std::string _request_query;	// Optional.
 
-		// All methods must contain a "Host" field.
+		// Header fields in format "key:value".
+		//
+		// All requests must contain a "Host" field.
 		// Without it, the server should respond with 400.
 		//
-		// Additionally, POST must also include
-		// either the "Content-Length" or "Transfer-Encoding"
-		// field.
-		// Without it, the server should respond with 411.
+		// Additionally, POST method must also include
+		// either the "Content-Length" or "Transfer-Encoding" field.
+		// If both are present, "Transfer-Encoding" takes precedence.
+		// Without both, the server should respond with 411.
 		std::map<std::string, std::string> _header_fields;
 
 		bool _complete;			// If request is fully parsed.
@@ -88,5 +102,49 @@ class HTTPRequest
 		HTTPRequest(const HTTPRequest &src);
 		HTTPRequest &operator = (const HTTPRequest &src);
 
-		//bool set_method(const std::string &info);
+		/**
+		 * Set the method of the request, depending on the value
+		 * stored in \p start_line.
+		 * @throw	invalid_argument	The request method
+		 * 					isn't supported.
+		 * @param	start_line	The start line of the request
+		 * 				with the "\r\n" erased.
+		 * @return	Processed bytes in \p start_line.
+		 */
+		size_t set_method(const std::string &start_line);
+
+		/**
+		 * Set the `_request_target` and `_request_query`
+		 * to those from \p start_line.
+		 * @param	start_line	The start line of the request
+		 * 				with the "\r\n" erased.
+		 * @param	pos		Where the request target
+		 * 				in \p start_line starts.
+		 * @return	Processed bytes in \p start_line.
+		 */
+		size_t set_request_target_and_query(const std::string &start_line,
+				size_t pos);
+
+		/**
+		 * Parse method, request target and
+		 * optional request query (if present) from \p start_line.
+		 * @throw	invalid_argument	The start line stored
+		 * 					in \p info is malformed.
+		 * @param	start_line	The start line of the request
+		 * 				with the "\r\n" erased.
+		 * @return	Processed bytes in \p start_line.
+		 */
+		size_t handle_start_line(const std::string &start_line);
+
+		/**
+		 * Parse header field from \p info
+		 * and store the result in `_header_fields`., request target and
+		 * optional request query (if present) from \p info.
+		 * @throw	invalid_argument	The start line stored
+		 * 					in \p info is malformed.
+		 * @param	info	The start line of the request
+		 * 			with the "\r\n" erased.
+		 * @return	Processed bytes in \p info (excluding "\r\n").
+		 */
+		size_t handle_header_field(const std::string &info);
 };
--- src/HTTPRequest.cpp
+++ src/HTTPRequest.cpp
@@ -2,6 +2,7 @@
 #include <cstddef>
 #include <string>
 #include <stdexcept>
+#include <vector>
 
 HTTPRequest::HTTPRequest()
 	: _complete(false)
@@ -14,22 +15,76 @@ HTTPRequest::~HTTPRequest()
 
 size_t HTTPRequest::process_info(const std::string &info)
 {
-	size_t nl_ft_pos;		// Position of '\n' in "\r\n" terminator.
-	const size_t BEGINNING = 1;	// To check for "\r\n" terminator.
+	size_t ft_pos;	// Field terminator position.
+	const std::string FIELD_TERMINATOR = "\r\n";
 
 	if (this->_complete)
 	{
 		throw std::range_error("HTTPRequest::process_info(): Request has already been fully parsed.");
 	}
-	nl_ft_pos = info.find('\n');
-	if (nl_ft_pos == std::string::npos
-		|| nl_ft_pos == 0 || info.at(nl_ft_pos - 1) != '\r')
+	ft_pos = info.find(FIELD_TERMINATOR);
+	if (ft_pos == std::string::npos)
 	{
 		throw std::invalid_argument("HTTPRequest::process_info(): Some line isn't properly terminated.");
 	}
-	else if (nl_ft_pos == BEGINNING && !(this->_method_set))
+	else if (ft_pos == 0)
 	{
-		throw std::runtime_error("HTTPRequest::process_info(): Found \"r\"n immediately after the request beginning.");
+		if (!(this->_method_is_set))
+		{
+			throw std::invalid_argument("HTTPRequest::process_info(): Found \"r\"n immediately after the request beginning.");
+		}
+		this->_complete = true;
+		return FIELD_TERMINATOR.length();
 	}
-	return 0;
+	if (!(this->_method_is_set) || this->_request_target.empty())
+	{
+		return this->handle_start_line(info.substr(0, ft_pos))
+			+ FIELD_TERMINATOR.length();
+	}
+	else
+	{
+		return this->handle_header_field(info.substr(0, ft_pos))
+			+ FIELD_TERMINATOR.length();
+	}
+}
+
+size_t HTTPRequest::set_method(const std::string &start_line)
+{
+	const std::string GET_STR = "GET", POST_STR = "POST", DELETE_STR = "DELETE";
+
+	if (start_line.compare(0, GET_STR.length(), GET_STR) == 0)
+	{
+		_method = GET;
+		_method_is_set = true;
+		return GET_STR.length();
+	}
+	else if (start_line.compare(0, POST_STR.length(), POST_STR) == 0)
+	{
+		_method = POST;
+		_method_is_set = true;
+		return POST_STR.length();
+	}
+	else if (start_line.compare(0, DELETE_STR.length(), DELETE_STR) == 0)
+	{
+		_method = DELETE;
+		_method_is_set = true;
+		return DELETE_STR.length();
+	}
+	throw std::invalid_argument("HTTPRequest::set_method(): Request method isn't supported.");
+}
+
+size_t HTTPRequest::handle_start_line(const std::string &start_line)
+{
+	size_t i;
+	const std::string AFTER_METHOD = " /";
+
+	i = this->set_method(start_line);
+	// " /" after the request method.
+	if (start_line.length() <= i
+		|| start_line.compare(i, AFTER_METHOD.length(), AFTER_METHOD) != 0)
+	{
+		throw std::invalid_argument("HTTPRequest::handle_start_line(): Start line is malformed.");
+	}
+	i += AFTER_METHOD.length();
+	return i;
 }
