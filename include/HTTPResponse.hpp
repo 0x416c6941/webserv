28d4a6f6ac18d80ba749c6e25919bd4d91add062
hlyshchu <hlyshchu@student.42prague.com>
2025-06-20 16:13:10 +0200

    HTTPResponse in progress. Vurrently can sent only default error pages

---
+++ include/HTTPResponse.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include "Webserv.hpp"
+#include "HTTPRequest.hpp"
+#include "ServerConfig.hpp"
+#include "MIME.hpp"
+
+/**
+ * @brief The HTTPResponse class is responsible for constructing a 
+ * HTTP response message (status line, headers, and body) 
+ * based on the request and server configuration.
+ * 
+ */
+class HTTPResponse {
+public:
+	HTTPResponse();
+	// HTTPResponse(const HTTPRequest& request, const ServerConfig& config);
+	~HTTPResponse();
+
+
+	// void prepare();
+	// std::string get_header_string() const;
+	// int get_status_code() const;
+	// std::string get_file_path() const;
+
+
+	void 			set_status_code(int code);
+	void 			build_error_response();
+	bool 			is_response_ready() const;
+	std::string 		get_response_msg() const;
+
+	void			reset();
+
+private:
+	std::string 		_response_msg;
+	// const HTTPRequest*      _request;
+	const ServerConfig*     _server_config;
+
+	int                     _status_code;
+	bool 		  	_response_ready;
+	// std::map<std::string, std::string> _headers;
+	// std::string             _resolved_path;
+
+	// void determine_status();
+	// void resolve_path();
+	// void build_headers();
+	// void use_error_page_if_needed();
+};
---
+++ src/HTTPResponse.cpp
@@ -0,0 +1,138 @@
+#include "../include/HTTPResponse.hpp"
+
+HTTPResponse::HTTPResponse():
+        _response_msg(""),
+        // _request(NULL),
+        _server_config(NULL),
+        _status_code(200),
+        _response_ready(false) {}
+
+// HTTPResponse::HTTPResponse(const HTTPRequest& request, const ServerConfig& config)
+// : _request(request), _server_config(config), _status_code(200)  {}
+
+HTTPResponse::~HTTPResponse() {}
+
+void HTTPResponse::set_status_code(int code)
+{
+        _status_code = code;
+}
+
+bool HTTPResponse::is_response_ready() const
+{
+        return _response_ready;
+}
+
+std::string HTTPResponse::get_response_msg() const
+{
+        return _response_msg;
+}
+
+
+void HTTPResponse::build_error_response()
+{
+        if(_server_config){
+                std::map<int, std::string>::const_iterator it = _server_config->getErrorPages().find(_status_code);
+                if (it != _server_config->getErrorPages().end()) {
+                       // If an error page is defined for this status code, use it
+                //        _response_ready = true;
+                }
+        }
+        else {
+                _response_msg = generateErrorPage(_status_code);
+                _response_ready = true;
+        }
+}
+
+
+// void HTTPResponse::prepare() {
+// 	determine_status();
+// 	resolve_path();
+// 	use_error_page_if_needed();
+// 	build_headers();
+// }
+
+// void HTTPResponse::determine_status() {
+// 	if (!_request.is_complete()) {
+// 		_status_code = 400;
+// 		_reason_phrase = "Bad Request";
+// 		return;
+// 	}
+
+// 	if (_request.get_method() != HTTPRequest::GET &&
+// 		_request.get_method() != HTTPRequest::POST &&
+// 		_request.get_method() != HTTPRequest::DELETE) {
+// 		_status_code = 405;
+// 		_reason_phrase = "Method Not Allowed";
+// 		return;
+// 	}
+
+// 	// Additional logic for POST content-length or transfer-encoding
+// 	if (_request.get_method() == HTTPRequest::POST) {
+// 		if (_request.get_header_value("Content-Length").empty() &&
+// 			_request.get_header_value("Transfer-Encoding").empty()) {
+// 			_status_code = 411;
+// 			_reason_phrase = "Length Required";
+// 		}
+// 	}
+// }
+
+// void HTTPResponse::resolve_path() {
+// 	if (_status_code != 200)
+// 		return;
+
+// 	_resolved_path = _config.getRoot() + _request.get_request_target();
+
+// 	struct stat sb;
+// 	if (stat(_resolved_path.c_str(), &sb) != 0 || S_ISDIR(sb.st_mode)) {
+// 		_status_code = 404;
+// 		_reason_phrase = "Not Found";
+// 		return;
+// 	}
+// }
+
+// void HTTPResponse::use_error_page_if_needed() {
+// 	if (_status_code == 200)
+// 		return;
+
+// 	std::map<int, std::string>::const_iterator it = _config.getErrorPages().find(_status_code);
+// 	if (it != _config.getErrorPages().end()) {
+// 		_resolved_path = _config.getRoot() + it->second;
+// 	}
+// }
+
+// void HTTPResponse::build_headers() {
+// 	_headers["Server"] = "Webserv/1.0";
+// 	_headers["Connection"] = "close";
+// 	_headers["Content-Type"] = MIME::get_type(_resolved_path);
+
+// 	struct stat sb;
+// 	if (stat(_resolved_path.c_str(), &sb) == 0)
+// 		_headers["Content-Length"] = std::to_string(sb.st_size);
+// }
+
+// std::string HTTPResponse::get_header_string() const {
+// 	std::string response = "HTTP/1.1 " + std::to_string(_status_code) + " " + _reason_phrase + "\r\n";
+// 	for (std::map<std::string, std::string>::const_iterator it = _headers.begin(); it != _headers.end(); ++it) {
+// 		response += it->first + ": " + it->second + "\r\n";
+// 	}
+// 	response += "\r\n";
+// 	return response;
+// }
+
+// int HTTPResponse::get_status_code() const {
+// 	return _status_code;
+// }
+
+// std::string HTTPResponse::get_file_path() const {
+// 	return _resolved_path;
+// }
+
+
+
+void    HTTPResponse::reset()
+{
+        _response_msg.clear();
+        _status_code = 200;
+        _response_ready = false;
+}
+
