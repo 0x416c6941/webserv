41bf89bdb49c5c491f03ff456303547abed6be33
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-07-07 15:25:02 +0200

    HTTPResponse: New design (nothing works)

--- include/HTTPResponse.hpp
+++ include/HTTPResponse.hpp
@@ -11,26 +11,17 @@
 class HTTPResponse {
 public:
 	HTTPResponse();
+	HTTPResponse(int status_code);
 	HTTPResponse(const HTTPResponse& other);
-	// HTTPResponse(const HTTPRequest& request, const ServerConfig& config);
 	HTTPResponse& operator=(const HTTPResponse& other);
 	~HTTPResponse();
-
-
-	// void prepare();
-	// std::string get_header_string() const;
-	// int get_status_code() const;
-	// std::string get_file_path() const;
-
-	void 			set_root(const std::string& root);
-	void 			set_status_code(int code);
-	void 			build_error_response(const ServerConfig& server_config);
-	bool 			is_response_ready() const;
-	std::string 		get_response_msg() const;
-	void 			handle_response_routine(const ServerConfig& server_config, const HTTPRequest& request);
 	void			reset();
 
-public:
+	void 			build_error_response(const ServerConfig &server_config);
+	void 			handle_response_routine(const ServerConfig &server_config, const HTTPRequest &request);
+	bool 			is_response_ready() const;
+	std::string		get_response_msg() const;
+
 	class HTTPError : public std::exception {
 		public:
 			// Those shouldn't be public...
@@ -43,41 +34,13 @@ public:
 			virtual const char* what() const throw();
 	};
 
-
 private:
-	std::string 				_mime;
-	std::string 				_response_msg;
-	std::string 				_response_body;
-	std::string 				_root;
-	int                     		_status_code;
+	int					_status_code;
+	// std::map<std::string, std::string>	_headers;
+	std::string				_response_body;
 	bool 		  			_response_ready;
-	// std::map<std::string, std::string> _headers;
-	// std::string             _resolved_path;
 
-	// helpers
-	bool 		has_permission(const std::string& path, HTTPRequest::e_method method) const;
-	std::string 	resolve_secure_path(const std::string& request_path) const;
-
-	/**
- 	* @brief Validates an HTTP request according to HTTP/1.1 protocol rules.
- 	*
- 	* This function checks the structural validity and completeness of the request.
- 	* It ensures that:
- 	* - The request is fully parsed.
- 	* - The "Host" header is present (as required by HTTP/1.1).
- 	* - For POST requests, either "Content-Length" or "Transfer-Encoding" is provided.
- 	*
- 	* If the request is invalid, the appropriate HTTP status code is set:
- 	* - 400 Bad Request: if the request is incomplete or malformed.
- 	* - 405 Method Not Allowed: if the method is not supported.
- 	* - 411 Length Required: if a POST request lacks both length and transfer encoding headers.
- 	*
- 	* @param request A constant reference to the HTTPRequest to be validated.
- 	* @return true if the request is valid and all required headers/methods are satisfied; false otherwise.
-	*/
-	bool		validate_request(const HTTPRequest& request);
-
-	std::string 	build_response_msg() const;
 	std::string 	get_mime_type(const std::string &path);
 	void 		handleGET(const HTTPRequest& request, const ServerConfig& server_config);
+	// handlePOST(), handleDELETE().
 };
