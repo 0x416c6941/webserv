0x416c6941/webserv

commit ee11f7c358ec13c9030f128a0500e2f5892114da

ServerBuilder::build(): Throw exceptions now if amount of CGI paths isn't equal to amount of CGI exts

src/ServerBuilder.cpp
@@ -686,5 +686,18 @@ ServerConfig ServerBuilder::build(const std::vector<std::string>& directives) {
 			handler(tokens, server_cfg);
 		}
 	}
+	// CGI paths count must correspond to count of CGI extensions.
+	// Basically, we provide a path to a handler to each CGI extension type.
+	for (std::vector<Location>::const_iterator it = server_cfg.getLocations().begin();
+		it != server_cfg.getLocations().end(); ++it)
+	{
+		if (it->getCgiPath().size() != it->getCgiExtension().size())
+		{
+			throw ConfigParser::ErrorException("ServerBuilder::build(): "
+					+ "Location \"" + it->getPath()
+					+ "\": amount of CGI paths isn't equal to "
+					+ "amount of CGI extensions provided.");
+		}
+	}
 	return server_cfg;
 }

include/Location.hpp
@@ -19,9 +19,11 @@ private:
 	std::pair<int, std::string> 	_return;
 	std::string 			_alias;
 	uint64_t 			_client_max_body_size;
+
 	// We need to keep track if `_client_max_body_size`
 	// was set to 0 or not set at all.
 	bool				_client_max_body_size_set;
+
 	std::vector<std::string> 	_cgi_path;
 	std::vector<std::string> 	_cgi_ext;
 	std::map<int, std::string> 	_error_pages;

commit 440aa7fc1193d0f5623a371cd15ddcaea31a23e2

ServerBuilder::build(): Fix borked exception string iniitalizer introduced in ee11f7c

src/ServerBuilder.cpp
@@ -693,7 +693,7 @@ ServerConfig ServerBuilder::build(const std::vector<std::string>& directives) {
 	{
 		if (it->getCgiPath().size() != it->getCgiExtension().size())
 		{
-			throw ConfigParser::ErrorException("ServerBuilder::build(): "
+			throw ConfigParser::ErrorException(std::string("ServerBuilder::build(): ")
 					+ "Location \"" + it->getPath()
 					+ "\": amount of CGI paths isn't equal to "
 					+ "amount of CGI extensions provided.");