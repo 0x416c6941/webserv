604c82190225865aa435244bd14e741102beb853
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-07-18 14:37:59 +0200

    Update data/cgi-bin/hello_name.sh

--- data/cgi-bin/hello_name.sh
+++ data/cgi-bin/hello_name.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-echo "Content-Type: text/html"
-echo ""
+# What we just received from the client.
+RESPONSE_BODY="<html><body>$(cat)</body></html>\n"
 
-echo "<html><body>"
-echo "<h1>POST received</h1>"
-echo "<pre>"
-
-# Read input data from stdin
-cat
-
-echo "</pre>"
-echo "</body></html>"
+echo -ne "HTTP/1.1 200 OK\r\n"
+echo -ne "Connection: close\r\n"
+echo -ne "Content-Length: ${#RESPONSE_BODY}\r\n"
+echo -ne "Content-Type: text/html\r\n"
+echo -ne "Server: $SERVER_NAME\r\n"
+echo -ne "\r\n"
+echo -ne "$RESPONSE_BODY"
